d1631556d25749233bb5b87c202f91cc5a66300f
Shane Chen <mike.chen1632@gmail.com>
2026-02-25 18:53:05 +0000

    added stripe source yml and payment sql scripts

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,8 +1,22 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status,
-    _ETL_LOADED_AT
+with 
 
-from {{ source('jaffle_shop', 'orders') }}
+source as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+renamed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status as order_status,
+        _etl_loaded_at
+
+    from source
+
+)
+
+select * from renamed
