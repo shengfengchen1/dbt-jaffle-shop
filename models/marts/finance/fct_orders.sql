db06178ccf4f9fa0b2cc29346a49ada407ae610d
Shane Chen <mike.chen1632@gmail.com>
2026-03-04 00:26:20 +0000

    added incremental model

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,34 +1,37 @@
-with orders as (
+{{
+    config(
+        materialized="incremental"
+    )
+}}
 
-    select * from {{ ref('stg_jaffle_shop__orders') }}
+with
+    orders as (select * from {{ ref("stg_jaffle_shop__orders") }}),
 
-),
+    payments as (select * from {{ ref("stg_stripe__payment") }}),
 
-payments as (
-    select * from {{ ref('stg_stripe__payment') }}
-),
+    order_payments as (
+        select order_id, sum(case when status = 'success' then amount end) as amount
 
-order_payments as (
-    select
-        order_id,
-        sum (case when status = 'success' then amount end) as amount
+        from payments
+        group by 1
+    ),
 
-    from payments
-    group by 1
-),
+    final as (
 
-final as (
+        select
+            orders.order_id,
+            orders.customer_id,
+            orders.order_date,
+            coalesce(order_payments.amount, 0) as amount
 
-    select
-        orders.order_id,
-        orders.customer_id,
-        orders.order_date,
-        coalesce (order_payments.amount, 0) as amount
-
-    from orders
-    left join order_payments using (order_id)
-)
-
+        from orders
+        left join order_payments using (order_id)
+    )
 
 select *
 from final
+{% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    where order_date >= (select max(order_date) from {{ this }})
+{% endif %}
+order by order_date desc
